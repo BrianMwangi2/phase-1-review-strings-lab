701dcc59d4744f36681ce28962d4a334a1e99a2c
Brian <brian.mwangi2@student.moringaschool.com>
2024-03-19 21:28:14 +0300

    adds content to files

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+/*
+let currentUser=`Brian`;
+
+console.log(currentUser);
+
+let welcomeMessage="Welcome to Flatbook, ";
+ welcomeMessage="Welcome to Flatbook," `${currentUser}`;
+ console.log(welcomeMessage);
+*/
+const currentUser = `Brian`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage =  welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
